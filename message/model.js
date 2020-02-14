cdb5f0d7cfd41678d56883b6ef8d196001a774d0
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-14 11:28:34 +0100

    defined db schema and routed

---
+++ message/model.js
@@ -0,0 +1,9 @@
+const db = require("../db");
+
+const Sequelize = require("sequelize");
+
+const Message = db.define("message", {
+  text: Sequelize.STRING
+});
+
+module.exports = Message;
--- message/router.js
+++ message/router.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const { Router } = express;
 const router = Router();
+const Message = require("./model");
 
-router.post("/message", function(request, response) {
-  const { body } = request;
-  console.log("Request.body test: ", body);
+router.post("/message", async function(request, response, next) {
+  try {
+    const { body } = request;
+    const { text } = body;
+    const entity = { text };
+    await Message.create(entity);
+    response.status(200).json(entity);
+    console.log("Request.body test: ", body);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
