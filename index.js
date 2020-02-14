ae24fa6e22a1b99439b689bae2f01fb1e2690132
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-14 10:59:51 +0100

    added message endpoint

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-
 const app = express();
-
 const port = 4000;
+const jsonMiddleware = express.json();
+app.use(jsonMiddleware);
+
+app.post("/message", function(request, response) {
+  const { body } = request;
+  console.log("Request.body test: ", body);
+});
 
 function onListen() {
   console.log("Listening on port:", port);
 }
-
 app.listen(port, onListen);
